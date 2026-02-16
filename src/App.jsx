cc47d470a878fc3cf11e14c24e86765181e44602
srs <srihitha@gmail.com>
2026-02-16 10:21:09 +0530

    this is a my project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>count {count}</h1>
+        <div>
+          <button onClick={() => setCount(count+1)}>Increase</button>
+          <button onClick={() => setCount(count-1)}>Decrease</button>
+          <button onClick={() => setCount(0)}>Reset</button>
+        </div>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
